3f2aeecd2892060365c93c9cfcf2afe173e602a2
stefanacerovina <stefana@basiq.io>
2019-10-01 14:49:05 +0200

    Changed version

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from "./App";
 import "./styles/Layout.css";
 
 // eslint-disable-next-line no-console
-console.log("Basiq Connect Control v1.0.12");
+console.log("Basiq Connect Control v1.0.14");
 
 ElementQueries.listen();
 ElementQueries.init();
