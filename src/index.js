aac07cb22a362b887113153213ff149417bde64c
Mario Vukašinović <mario@basiq.io>
2019-10-03 10:44:30 +0200

    Thumbnails width.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from "./App";
 import "./styles/Layout.css";
 
 // eslint-disable-next-line no-console
-console.log("Basiq Connect Control v1.0.17");
+console.log("Basiq Connect Control v1.0.18");
 
 ElementQueries.listen();
 ElementQueries.init();
